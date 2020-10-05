7391d2f2dbf7b7e30f5fb6f6b70b82e217a76ea8
Jasen Jacobsen <jasen.robert.jacobsen@gmail.com>
2020-10-05 09:27:36 -0700

    Updated delta variables to diff then mask

--- Exact_Volume_Budget/Dev_SaltDeriv_TimeSteping.py
+++ Exact_Volume_Budget/Dev_SaltDeriv_TimeSteping.py
@@ -16,23 +16,24 @@ from netCDF4 import Dataset as nc4
 import Differential as dff
 
 #bounds of control volume
-latbounds = [35, 37]
-lonbounds = [-126, -125]
+latbounds = [37.193, 37.378]
+lonbounds = [-122.7, -122.456]
 
 #load files
-FilePath = '/home/cae/runs/jasen/wc15.a01.b03.hourlywindWT.windcurrent.diags/out/'
-HistFile = FilePath + 'ocean_his_2014_0006.nc'
+FilePath = '/home/cae/runs/jasen/wc15.a01.b03.hourlywindWT.windmcurrent.diags/out/'
+HistFile = FilePath + 'ocean_his_2014_0005.nc'
 Hist = nc4(HistFile, 'r')
 
-AvgFile = FilePath + 'ocean_avg_2014_0006.nc'
+AvgFile = FilePath + 'ocean_avg_2014_0005.nc'
 Avg = nc4(AvgFile, 'r')
 
-Diag = nc4(FilePath + 'ocean_dia_2014_0006.nc', 'r')
+Diag = nc4(FilePath + 'ocean_dia_2014_0005.nc', 'r')
 
 Grd = nc4('/home/ablowe/runs/ncfiles/grids/wc15.a01.b03_grd.nc', 'r')
 
 #create CV mask
-RhoMask = rt.RhoMask(AvgFile, latbounds, lonbounds)
+RhoMask2D = rt.RhoMask(AvgFile, latbounds, lonbounds)
+RhoMask = np.repeat(np.array(RhoMask2D)[np.newaxis, :, :], axis = 0)
 
 #load time for steping
 time = Avg.variables['ocean_time'][:]
@@ -46,29 +47,31 @@ SprInt.fill(np.nan)
 for t in range(time.shape[0]) :
     #avg file variables at t
     salt = ma.array(Avg.variables['salt'][t, :, :, :], mask = RhoMask)
-    s_prime = salt - salt.mean()    
-    deltaA = ma.diff(ma.array(dep._set_depth(AvgFile, None, 'w', \
+    s_prime = salt - salt.mean()
+    
+    #diff and mask are in opposite order    
+    deltaA = ma.array(ma.diff(dep._set_depth(AvgFile, None, 'w', \
                                              Avg.variables['h'][:], \
-                                             Avg.variables['zeta'][t, :, :]), \
-                              mask = RhoMask),\
-                     n = 1, axis = 1)
+                                             Avg.variables['zeta'][t,:,:]),
+                              n = 1, axis = 1), \
+                      mask = RhoMask)
     
     #diag file variables at t
     salt_rate = ma.array(Diag.variables['salt_rate'][t, :, :, :], \
                          mask = RhoMask)
     
     #hist variables at t and t+1
-    deltaH0 = ma.diff(ma.array(dep._set_depth(HistFile, None, 'w', \
-                                              Hist.variables['h'][:], \
-                                              Hist.variables['zeta'][t, :, :]),\
-                               mask = RhoMask), \
-                      n = 1, axis = 1)
+    deltaH0 = ma.array(ma.diff(dep._set_depth(HistFile, None, 'w', \
+                                             Hist.variables['h'][:], \
+                                             Hist.variables['zeta'][t,:,:]),
+                              n = 1, axis = 1), \
+                      mask = RhoMask)
     
-    deltaH1 = ma.diff(ma.array(dep._set_depth(HistFile, None, 'w', \
-                                               Hist.variables['h'][:], \
-                                               Hist.variables['zeta'][t+1, :, :]),\
-                                mask = RhoMask),\
-                      n = 1, axis = 1)
+    deltaH1 = ma.array(ma.diff(dep._set_depth(HistFile, None, 'w', \
+                                             Hist.variables['h'][:], \
+                                             Hist.variables['zeta'][t+1,:,:]),
+                              n = 1, axis = 1), \
+                      mask = RhoMask)
     
     dtH = Hist.variables['ocean_time'][t+1] - Hist.variables['ocean_time'][t]
     
